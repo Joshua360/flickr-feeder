e5cd5a5ddd2e9e69accd17f31e89c2407362d739
Joshua360 <joshuaowuor27@gmail.com>
2022-06-06 16:36:03 +0300

    accepted incoming change

--- js/app.js
+++ js/app.js
@@ -1,35 +1,6 @@
 $(document).ready(function() {
 
 
-<<<<<<< HEAD
- $('form').submit(function(e){
- e.preventDefault();
-  let $searchTerm = $('#search').val();
-  
-
-    // the AJAX part
-    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    var animal = $searchTerm;
-    var flickrOptions = {
-      tags: animal,
-      format: "json"
-    };
-    function displayPhotos(data) {
-      var photoHTML = '<ul>';
-      $.each(data.items,function(i,photo) {
-        photoHTML += '<li class="grid-25 tablet-grid-50">';
-        photoHTML += '<a href="' + photo.link + '" class="image">';
-        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
-      }); // end each
-      photoHTML += '</ul>';
-      $('#photos').html(photoHTML);
-    }
-    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
-
-  }); // end click
-
-}); // end ready 
-=======
   $('form').submit(function(e){
   e.preventDefault();
    let $searchTerm = $('#search').val();
@@ -57,4 +28,3 @@ $(document).ready(function() {
    }); // end click
  
  }); // end ready
->>>>>>> 56ba2e8d559d1102f11158eef89e746f5b51237b
